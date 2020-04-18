3d22bb0ef22687828ffb65270ac02d320a2bd2b0
Oleg Simonov <simonovo@byu.edu>
2020-04-18 10:39:05 -0600

    Back end done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,9 +22,10 @@ namespace SeeThisMovieQuestionMark.Controllers
 
         //INDEX POST
         [HttpPost]
-        public ActionResult Index(FormCollection form)
+        public ActionResult Result(FormCollection form)
         {
             int mylevel;
+            string title;
             string genres;
             int duration;
             string country;
@@ -32,7 +33,7 @@ namespace SeeThisMovieQuestionMark.Controllers
             string description;
 
             mylevel = Int32.Parse(form["mylevel"]);
-
+            title = form["title"];
             genres = form["genre1"];
             if(form["genre2"] != "")
             {
@@ -55,27 +56,38 @@ namespace SeeThisMovieQuestionMark.Controllers
             request.AddHeader("Authorization", "Bearer 7ZlkHBdNVikj14uU99/pyfAsj7+KbnCZ42DkMqyOMud3Mx9JFVY/Q4B1dUvzduEj6jmYlG57rgisFQ1I+YnioQ==");
             request.AddHeader("Content-Type", "application/json");
             request.AddHeader("Content-Type", "application/json");
-            request.AddParameter("application/json,application/json", "{\r\n  \"Inputs\": {\r\n    \"input1\": {\r\n      \"ColumnNames\": [\r\n        \"genre\",\r\n        \"duration\",\r\n        \"country\",\r\n        \"language\",\r\n        \"description\",\r\n        \"avg_vote\"\r\n      ],\r\n      \"Values\": [\r\n        [\r\n          \"" + genres + "\",\r\n          \"" + duration + "\",\r\n          \"" + country + "\",\r\n          \"" + language + "\",\r\n          \"" + description + "\",\r\n          \"\"\r\n        ]\r\n      ]\r\n    }\r\n  },\r\n  \"GlobalParameters\": {}\r\n}", ParameterType.RequestBody);
+            request.AddParameter("application/json,application/json", "{\r\n  \"Inputs\": {\r\n    \"input1\": {\r\n      \"ColumnNames\": [\r\n        \"genre\",\r\n        \"duration\",\r\n        \"country\",\r\n        \"language\",\r\n        \"description\",\r\n        \"avg_vote\"\r\n      ],\r\n      \"Values\": [\r\n        [\r\n          \"" + genres + "\",\r\n          \"" + duration + "\",\r\n          \"" + country + "\",\r\n          \"" + language + "\",\r\n          \"" + description + "\",\r\n          \"0\"\r\n        ]\r\n      ]\r\n    }\r\n  },\r\n  \"GlobalParameters\": {}\r\n}", ParameterType.RequestBody);
             IRestResponse response = client.Execute(request);
             var result = JObject.Parse(response.Content);
-            //string prediction = result["Results"]["output1"]["value"]["Values"].ToString().Replace("[", "").Replace("]", "").Replace("\"", "");
-            //float num_prediction = Convert.ToSingle(prediction);
-            ViewBag.CalcResponse = "This movie's rating will be close to " + result; // eventually, this value will be returned from our Azure webservice
+            string prediction = result["Results"]["output1"]["value"]["Values"].ToString().Replace("[", "").Replace("]", "").Replace("\"", "");
+            System.Double num_prediction = Math.Round(Convert.ToDouble(prediction),2);
+            ViewBag.PredictedAvgVote = "This movie's rating will be close to " + num_prediction;
+
+            ViewBag.PickLevel = mylevel;
+
+            if (num_prediction >= mylevel)
+            {
+                ViewBag.Decision = "You should give this movie a shot";
+            }
+            else
+            {
+                ViewBag.Decision = "This movie will probably not satisfy you";
+            }
 
             try
             {
                 string title_query = "SELECT title FROM movies WHERE avg_vote >" + mylevel + "ORDER BY NEWID()"; 
-                var title = db.Database.SqlQuery<string>(title_query).FirstOrDefault().ToString();
-                string year_query = "SELECT year FROM movies WHERE title='" + title + "'";
+                var suggested_title = db.Database.SqlQuery<string>(title_query).FirstOrDefault().ToString();
+                string year_query = "SELECT year FROM movies WHERE title='" + suggested_title + "'";
                 var year = db.Database.SqlQuery<int>(year_query).FirstOrDefault().ToString();
-                ViewBag.Suggestion = ("We think you might like this movie: " + title + " (" + year + ")");
+                ViewBag.Suggestion = ("We think you might also like this movie: " + suggested_title + " (" + year + ")");
             }
             catch (Exception e)
             {
                 ViewBag.Suggestion = "Sorry, something went wrong, and we have nothing to suggest for you today! :(";
             }
 
-            return View();
+            return View(form);
         }
         public ActionResult About()
         {
